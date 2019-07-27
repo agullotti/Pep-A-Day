2d5a5fdc985bee011c7720f8c571fc243fe42464
agullotti <24842104+agullotti@users.noreply.github.com>
2019-07-27 18:33:41 -0400

    updated formatting
    tinkering with beautifulsoup to produce needed formatting

--- pep-a-day.py
+++ pep-a-day.py
@@ -7,17 +7,12 @@ from tkinter import *
 # gui information
 master = Tk()
 master.title("Pep-A-Day")
-master.configure(background='red')
-
 
 pep8_main = "https://www.python.org/dev/peps/"
 pep8_main_requests = requests.get(pep8_main)
 pep8_main_raw = pep8_main_requests.content
 
 soup = BeautifulSoup(pep8_main_raw, features="lxml")
-
-# Get Title Of Pep
-pep_title = soup.title.get_text()
 
 # Get Complete List of peps from index
 pep_list = [] # initialize blank list
@@ -42,15 +37,17 @@ else:
 pep_url = requests.get(random_url)
 pep_url_raw = pep_url.content
 pep_soup = BeautifulSoup(pep_url_raw, 'lxml')
+# Get Title Of Pep
+pep_title = pep_soup.title.get_text()
+# Find all body sections
 pep_soup_body = pep_soup.find_all(class_='section')
-# pep_text_t = pep_soup_body.get_text()
-# print(pep_text_t)
-list_test = []
-for s in pep_soup_body:
-    list_test_2 = s.get_text()
-    list_test.append(list_test_2)
 
-lt3 = ','.join(str(x) for x in list_test) 
+# Create blank list, get only text of section found then append to blank list
+pep_section_list = []
+for s in pep_soup_body:
+    pep_section_list2 = s.get_text()
+    pep_section_list.append(pep_section_list2)
+pep_section_list3 = ' '.join(str(x) for x in pep_section_list) 
 
 # Create Scroll Bar
 scroll=Scrollbar(master)
@@ -59,11 +56,12 @@ scroll.pack(side=RIGHT, fill=Y)
 # Create Text Widget for Pep
 gui_text = Text(master, wrap=NONE, yscrollcommand=scroll.set)
 gui_text.configure(background='#306998', foreground='#FFD43B')
-gui_text.insert("1.0", lt3)
+gui_text.insert("1.0", pep_section_list3)
 gui_text.pack(side=LEFT, expand=True, fill='both')
 
 # configure scrollbar for text widget
 scroll.configure(command=gui_text.yview)
+
 
 # start main
 mainloop()
